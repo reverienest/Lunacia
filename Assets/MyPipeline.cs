5db09713789939af3a98b9bdb3789e0259710127
zgoddard3 <zgoddard3@gatech.edu>
2020-09-10 21:21:34 -0400

    asdf

---
+++ Assets/MyPipeline.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MyPipeline : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/MyPipelineAsset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/MyRenderPipeline")]
+public class MyPipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new MyPipeline();
+    }
+}
