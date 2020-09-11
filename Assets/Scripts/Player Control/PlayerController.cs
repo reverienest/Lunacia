6ea28b98df95aca518b8be016729f15c1338e3ef
Arthur Clay Odom <thekingofthebritons@gmail.com>
2020-09-10 21:25:01 -0400

    Doesn't Work - Editor Problems REEE

---
+++ Assets/Scripts/Player Control/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float acceleration;
+    
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.getComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the Cursor Position relative to the player
+        Vector2 rel = Camera.ScreenToWorld(Input.mousePosition).subtract(transform.position);
+
+        if (Input.getMouseButton(0)) {
+            rigidbody.addForce(rel.normalized * acceleration, ForceMode.Impulse);
+        }
+    }
+}
