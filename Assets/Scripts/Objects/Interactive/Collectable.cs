f0e7c670d8e37ac9373f5a44ee2eb79f018edcd3
Daniel Carr <randomerz@github.com>
2020-09-13 00:58:44 -0400

    Added Basic Collectable system and light bobbing animation
    - added collectable strawberry and books which remove themselves after collection
    - created a lot of folders
    - added scene under "TestingScenes/Daniel Carr"
    - added basic Collectable script to be added to collectable objects
    - added PlayerInventory script to be added to player
    - added simple bobbing animations for the collectables
    - player has a circle collider added to it

---
+++ Assets/Scripts/Objects/Interactive/Collectable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public string CollectableType;
+    public GameObject parentCollectable;
+    // Strawberry, Book1, Book2;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.tag);
+        if (collision.tag == "Player")
+        {
+            PlayerInventory pInv = collision.gameObject.GetComponent<PlayerInventory>();
+            if (pInv != null)
+                Collect(pInv);
+            else
+                Debug.LogWarning("Player doesn't have a PlayerInventory script attatched");
+        }
+    }
+
+    private void Collect(PlayerInventory playerInv)
+    {
+        playerInv.CollectItem(CollectableType);
+
+        if (parentCollectable == null)
+            Destroy(gameObject);
+        else
+            Destroy(parentCollectable);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    // store collected in bool[]?
+    public int strawberryCount; // temp
+    public int book1Count;
+    public int book2Count;
+
+    void Start()
+    {
+        
+    }
+
+    public void CollectItem(string item)
+    {
+        Debug.Log("Collected " + item + "!");
+        switch (item)
+        {
+            case "Strawberry":
+                strawberryCount += 1;
+                break;
+            case "Book1":
+                book1Count += 1;
+                break;
+            case "Book2":
+                book2Count += 1;
+                break;
+        }
+    }
+}
