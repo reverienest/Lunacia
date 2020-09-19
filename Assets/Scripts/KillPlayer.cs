9c59d1c7f3ec84b21f5e8d3c6c518983d074b386
ZealousElite120 <skar32@gatech.edu>
2020-09-18 20:06:00 -0400

    Fixed bug with KillPlayer pubsub

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -2,37 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System;
 using Pubsub;
 
 public class KillPlayer : MonoBehaviour
 {
     GameObject player;
     GameObject[] hazards;
-    Scene currentScene;
 
-    bool sceneSwitched = false;
-    
-    void Awake() {
+    static KillPlayer()
+    {
         MessageBroker.Instance.playerDeath += consumePlayerDeathEvent;
     }
 
-    private void consumePlayerDeathEvent(object sender, PlayerDeathEvent death) {
+    static void consumePlayerDeathEvent(object sender, PlayerDeathEvent death) {
 		print(death.deathMessage);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        sceneSwitched = true;
 	}
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         hazards = GameObject.FindGameObjectsWithTag("Hazard");
-        currentScene = SceneManager.GetActiveScene();
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Hazard" && !sceneSwitched)
+        if (col.gameObject.tag == "Hazard")
         {
             MessageBroker.Instance.Raise(new PlayerDeathEvent("Player died!"));
         }
