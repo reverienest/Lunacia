ef5deea5d574325c666d770c88e0c0e114e4ced1
ZealousElite120 <skar32@gatech.edu>
2020-09-24 16:25:32 -0400

    Created respawn system

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -7,21 +7,21 @@ using Pubsub;
 public class KillPlayer : MonoBehaviour
 {
     public GameObject player;
-
+    public static Vector2 respawnLocation; // this will always spawn the player at (0,0) when first starting up the scene
     private static bool addedMessageBroker = false;
-
+   
     void Awake() {
-        if (!addedMessageBroker) {
+        if (!addedMessageBroker) { 
             MessageBroker.Instance.playerDeath += consumePlayerDeathEvent;
             addedMessageBroker = true;
         }
+        player.transform.position = respawnLocation;
     }
-
     static void consumePlayerDeathEvent(object sender, PlayerDeathEventArguments death) {
 		print(death.deathMessage);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("Respawned at: " + respawnLocation);
 	}
-
     void OnCollisionEnter2D(Collision2D col) {
         if (col.gameObject.tag == "Hazard")
         {
