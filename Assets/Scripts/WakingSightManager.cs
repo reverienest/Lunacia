25e1eee595e232ea409051aca347e067d1263fad
wendy326 <wssw2024@gmail.com>
2020-09-24 19:22:42 -0500

    Player HUD
    Location and Waking Sight displays

---
+++ Assets/Scripts/WakingSightManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WakingSightManager : MonoBehaviour
+{
+    // Drag image component here in the inspector window
+    public Image myImageComponent;
+
+    // Drag first sprite here in the inspector window
+    public Sprite WSoff;
+
+    // Drag second sprite here in the inspector window
+    public Sprite WSon;
+
+    public void SetImage(Sprite x)
+    {
+        this.GetComponent<Image>().sprite = x;
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SetImage(WSoff);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetImage(WSon);
+        }
+    }
+}
+
