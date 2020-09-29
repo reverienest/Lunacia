7801c92c369b8c71e102b0fd23ed35c98d97cf53
JacksonLeone <jackson.leone.usa@gmail.com>
2020-09-29 11:30:36 -0400

    DreamCurrents Prefab & SpeedyCameraScripts
    I have including in this push the prefab of dream current (found in Prefabs/Objects/Interactables) as well as the SpeedyCamera script which will zoom out a cinamachine virtual camera when a player goes above a certain speed (most likely in the gale and the dream currents).

---
+++ Assets/Scripts/Gale & Dream Currents/DreamCurrents.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+
+public class DreamCurrents : MonoBehaviour
+{
+    public string direction;
+    private Animator cameraAnimator;
+    private PlayerController player;
+    private AreaEffector2D force;
+
+    public float releaseTimer = 0.3f;
+    private float releaseTimerCount;
+    private bool timerActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraAnimator = FindObjectOfType<CinemachineVirtualCamera>().gameObject.GetComponent<Animator>();
+        force = GetComponent<AreaEffector2D>();
+        if (direction.Equals("right")) {
+            force.forceAngle = 0;
+        } else if (direction.Equals("up")) {
+            force.forceAngle = 90;
+        } else if (direction.Equals("left")) {
+            force.forceAngle = 180;
+        } else if (direction.Equals("down")) {
+            force.forceAngle = 270;
+        }
+        timerActive = false;
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerActive) {
+            releaseTimerCount -= Time.deltaTime;
+            if (releaseTimerCount < 0) {
+                cameraAnimator.Play("PlayerSlow");
+                timerActive = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            cameraAnimator.Play("PlayerBoost");
+            releaseTimerCount = releaseTimer;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            timerActive = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Gale & Dream Currents/SpeedyCameraScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+
+public class SpeedyCameraScript : MonoBehaviour
+{
+    private Animator cameraAnimator;
+    public bool isActive;
+
+    public float speedThreshold;
+    private Rigidbody2D playerRB;
+
+    // public float releaseTimer = 0.3f;
+    // private float releaseTimerCount;
+    // private bool timerActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraAnimator = FindObjectOfType<CinemachineVirtualCamera>().gameObject.GetComponent<Animator>();
+        playerRB = GetComponent<Rigidbody2D>();
+        // timerActive = false;
+        isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(playerRB.velocity.magnitude);
+        if (!isActive && playerRB.velocity.magnitude > speedThreshold) {
+            cameraAnimator.Play("PlayerBoost");
+            isActive = true;
+        }
+        if (isActive && playerRB.velocity.magnitude < speedThreshold) {
+            cameraAnimator.Play("PlayerSlow");
+            isActive = false;
+        }
+    }
+}
