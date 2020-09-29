fa25138de9faf4f53ce69052009b6e290517c198
JacksonLeone <jackson.leone.usa@gmail.com>
2020-09-29 12:59:11 -0400

    Gave Dream Currents Particle Effects
    I combined Zach's particle effects with my Dream Current Prefab and tweaked a bit of the DreamCurrent Script to make it easier to drag and drop the Dream Current Prefab

--- Assets/Scripts/Gale & Dream Currents/DreamCurrents.cs
+++ Assets/Scripts/Gale & Dream Currents/DreamCurrents.cs
@@ -6,55 +6,17 @@ using Cinemachine;
 
 public class DreamCurrents : MonoBehaviour
 {
-    public string direction;
-    private Animator cameraAnimator;
-    private PlayerController player;
+    public float ForceMagnitude;
     private AreaEffector2D force;
 
-    public float releaseTimer = 0.3f;
-    private float releaseTimerCount;
-    private bool timerActive;
+    private Transform position;
+
     // Start is called before the first frame update
     void Start()
     {
-        cameraAnimator = FindObjectOfType<CinemachineVirtualCamera>().gameObject.GetComponent<Animator>();
+        position = GetComponent<Transform>();
         force = GetComponent<AreaEffector2D>();
-        if (direction.Equals("right")) {
-            force.forceAngle = 0;
-        } else if (direction.Equals("up")) {
-            force.forceAngle = 90;
-        } else if (direction.Equals("left")) {
-            force.forceAngle = 180;
-        } else if (direction.Equals("down")) {
-            force.forceAngle = 270;
-        }
-        timerActive = false;
-         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (timerActive) {
-            releaseTimerCount -= Time.deltaTime;
-            if (releaseTimerCount < 0) {
-                cameraAnimator.Play("PlayerSlow");
-                timerActive = false;
-            }
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
-            cameraAnimator.Play("PlayerBoost");
-            releaseTimerCount = releaseTimer;
-        }
-    }
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            timerActive = true;
-        }
+        force.forceMagnitude = ForceMagnitude;
+        force.forceAngle = position.rotation.eulerAngles.z;
     }
 }
