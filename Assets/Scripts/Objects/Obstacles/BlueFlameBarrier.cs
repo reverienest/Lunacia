9562801910a55bf19a3339e60d7277689fed2f12
ZealousElite120 <skar32@gatech.edu>
2020-10-01 15:56:52 -0400

    Implemented Blazing Taiga flame barriers

---
+++ Assets/Scripts/Objects/Obstacles/BlueFlameBarrier.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueFlameBarrier : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update() {
+        if (KillPlayer.hasBlueFlame) {
+            this.GetComponent<BoxCollider2D>().isTrigger = true;
+        } else {
+            this.GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+    }
+}
