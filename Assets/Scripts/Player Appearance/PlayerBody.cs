b3341485901197754e4d03773dc3203d3f5d7830
aelu419 <atao9@gatech.edu>
2020-10-10 22:17:33 -0400

    warning fixes

--- Assets/Scripts/Player Appearance/PlayerBody.cs
+++ Assets/Scripts/Player Appearance/PlayerBody.cs
@@ -7,13 +7,13 @@ public class PlayerBody : MonoBehaviour
     public bool facingLeft = false;
 
     private Rigidbody2D rigid;
-    private SpriteRenderer renderer;
+    private SpriteRenderer renderer_;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponentInParent<Rigidbody2D>();
-        renderer = GetComponent<SpriteRenderer>();
+        renderer_ = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,7 +24,7 @@ public class PlayerBody : MonoBehaviour
         if (vel.x != 0)
         {
             facingLeft = vel.x < 0;
-            renderer.flipX = vel.x < 0;
+            renderer_.flipX = vel.x < 0;
             facingLeft = vel.x < 0.0f;
         }
         else
