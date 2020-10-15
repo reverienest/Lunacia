77fc8b05ca4767c365fd0e30422cc0569104deee
ZealousElite120 <skar32@gatech.edu>
2020-10-15 16:00:22 -0400

    Added trigger prefab to clear player flame

---
+++ Assets/Scripts/Player/ClearPlayerFlame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearPlayerFlame : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player" && (KillPlayer.hasBlueFlame || KillPlayer.hasRedFlame)) {
+            Destroy(col.gameObject.transform.GetChild(2).gameObject);
+            KillPlayer.hasBlueFlame = false;  
+            KillPlayer.hasRedFlame = false;  
+        }
+    }
+}
