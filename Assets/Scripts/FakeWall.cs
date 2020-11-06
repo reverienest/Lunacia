2e3ba120b50a419c5bea1eb6c72aa1277e86bfce
ZealousElite120 <skar32@gatech.edu>
2020-11-06 17:21:34 -0500

    Finished BT visual and functional implementation, added player death animation

---
+++ Assets/Scripts/FakeWall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Pubsub;
+
+public class FakeWall : MonoBehaviour
+{
+    void Awake() 
+    {
+        MessageBroker.Instance.WakingSightModeTopic += consumeWakingSightActiveEvent;
+    }
+
+    void consumeWakingSightActiveEvent(object sender, WakingSightModeEventArgs wakingSightState) {
+        print(wakingSightState.ActiveMode);
+        if (wakingSightState.ActiveMode == 1) {
+            GetComponent<TilemapCollider2D>().enabled = false;
+        } else if (wakingSightState.ActiveMode == 0) {
+            GetComponent<TilemapCollider2D>().enabled = true;
+        }
+    }
+}
