26595b4445d994430c64cf25bcc6f60838f353e5
Cinnamon18 <{11944881}+{Cinnamon18}@users.noreply.github.com>
2020-11-06 16:22:48 -0500

    remove broken imports

--- Assets/Scripts/FogEffect.cs
+++ Assets/Scripts/FogEffect.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Runtime.Remoting.Messaging;
 
 [ExecuteInEditMode]
 public class FogEffect : MonoBehaviour {
--- Assets/Scripts/UI/PauseMenuButtonController.cs
+++ Assets/Scripts/UI/PauseMenuButtonController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class PauseMenuButtonController : MonoBehaviour
