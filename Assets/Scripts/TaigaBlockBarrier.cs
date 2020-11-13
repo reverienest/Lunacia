91d2b64479f59533b37a0574024046e044ac3f60
ZealousElite120 <skar32@gatech.edu>
2020-11-13 04:22:59 -0500

    tweaked blazing taiga

---
+++ Assets/Scripts/TaigaBlockBarrier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaigaBlockBarrier : MonoBehaviour
+{
+    // dirty fix that prevents player from going through flame barriers in first part of taiga until waking sight upgrade is picked up
+    
+    public GameObject barrier;
+    public GameObject priorFlameBarrier;
+    public GameObject afterFlameBarrier;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player") {
+            Destroy(barrier);
+            priorFlameBarrier.SetActive(false);
+            afterFlameBarrier.SetActive(true);
+        }
+    }
+}
